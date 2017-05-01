09a1aa4092751c9a99be4cd66ea2471ee2ee05c4
Todor Petkov <tp.petkov89@gmail.com>
2017-05-01 20:58:47 +0300

    Bug fix and layout improved

---
+++ CarsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.FluentMVCTesting;
+
+namespace CarsStore.Tests
+{
+    using System.Web.Mvc;
+
+    using CarsStore.Web.Controllers;
+
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void IndexShouldPass()
+        {
+            var controller=new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Gosho",result.ViewBag);
+
+        }
+    }
+}
